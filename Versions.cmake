0d45ce10b3f0ebb0b37a88dae71a5c2b25383dd2
Arvo Järve <arvo.jarve@eesti.ee>
2020-05-07 12:21:29 +0300

    150

--- Versions.cmake
+++ Versions.cmake
@@ -2,5 +2,5 @@
 set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 1)
 set (WSJTX_VERSION_PATCH 0)
-set (WSJTX_RC 150_8)		 # release candidate number, comment out or zero for development versions
+set (WSJTX_RC 150)		 # release candidate number, comment out or zero for development versions
 set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
