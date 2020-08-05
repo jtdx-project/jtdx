1a4e04edfdc3fd8db7069f6095937877633505d2
Arvo Järve <arvo.jarve@eesti.ee>
2020-08-05 12:29:55 +0300

    update version

--- Versions.cmake
+++ Versions.cmake
@@ -2,5 +2,5 @@
 set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 2)
 set (WSJTX_VERSION_PATCH 0)
-set (WSJTX_RC 152_1)		 # release candidate number, comment out or zero for development versions
+set (WSJTX_RC 152_2)		 # release candidate number, comment out or zero for development versions
 set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
