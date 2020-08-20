6bfb0bdf29a158639bd18436db92b2fb2a9077f0
Igor Chernikov <c_igor@inbox.ru>
2020-08-20 22:19:04 +0300

    ALLCALL7 related capacity adjustment

--- lib/jt65_mod9.f90
+++ lib/jt65_mod9.f90
@@ -1,12 +1,10 @@
-! last time modified by Igor UA3DJY on 20200217
-
 module jt65_mod9 
 ! stores callsign DB in memory for false decodes checking
   
   parameter (MAXC=200000)
 ! supporting 7-char Australian callsigns
-  character*7 call0c(12000),calld(12000),callef(9000),callgh(7500),calli(7600),callj(8000),callk(23000),calllm(6000), &
-              calln(9500),callo(7000),callpq(6000),callr(6000),callst(7500),calluv(10500),callw(15000),callxz(5300)
+  character*7 call0c(11500),calld(11100),callef(9000),callgh(7500),calli(7600),callj(9000),callk(24000),calllm(6500), &
+              calln(9400),callo(6700),callpq(5800),callr(6500),callst(7500),calluv(10400),callw(13700),callxz(5800)
   character callsign*12
   character*180 line
   integer ncall0c,ncalld,ncallef,ncallgh,ncalli,ncallj,ncallk,ncalllm,ncalln,ncallo,ncallpq,ncallr,ncallst,ncalluv, &
