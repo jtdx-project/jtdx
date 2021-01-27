983554cde6e354ba2ef1428046ae4ed114cb9c1c
Arvo Järve <arvo.jarve@eesti.ee>
2021-01-27 10:56:39 +0200

    increase omp stack

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1001,7 +1001,7 @@ MainWindow::MainWindow(bool multiple, QSettings * settings, QSharedMemory *shdme
       , "-r", QDir::toNativeSeparators (m_config.data_dir ().absolutePath ())
       };
   QProcessEnvironment env {QProcessEnvironment::systemEnvironment ()};
-  env.insert ("OMP_STACKSIZE", "4M");
+  env.insert ("OMP_STACKSIZE", "5M");
   proc_jtdxjt9.setProcessEnvironment (env);
   proc_jtdxjt9.start(QDir::toNativeSeparators (m_appDir) + QDir::separator () +
           "jtdxjt9", jt9_args, QIODevice::ReadWrite | QIODevice::Unbuffered);
