faff18a83e617baf53609b3e9bf7f4a630488b00
Arvo Järve <arvo.jarve@eesti.ee>
2021-04-12 20:59:44 +0300

    update translations

--- translations/jtdx_es_ES.ts
+++ translations/jtdx_es_ES.ts
@@ -5059,8 +5059,8 @@ predefinida. La lista se puede actualizar en Ajustes (F2).</translation>
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;F3 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Mostrar los atajos de teclado&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;F4 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Borrar los campos Llamada Dx/Locator y Tx&lt;/td&gt;&lt;td&gt;&lt;b&gt;Alt+F4 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Salir del programa&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;F5 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Mostrar comandos especiales del ratón&lt;/td&gt;&lt;/tr&gt;
-  &lt;tr&gt;&lt;td&gt;&lt;b&gt;F6 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Abrir el siguiente archivo del directorio&lt;/td&gt;&lt;td&gt;&lt;b&gt;Mayúsculas+F6 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Desconectar todos los archivos restantes del directorio&lt;/td&gt;&lt;/tr&gt;.
-  &lt;tr&gt;&lt;td&gt;&lt;b&gt;F7 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Abrir el registro por asignado en el visor del sistema operativo&lt;/td&gt;&lt;/tr&gt;
+  &lt;tr&gt;&lt;td&gt;&lt;b&gt;F6 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Abrir el siguiente archivo del directorio&lt;/td&gt;&lt;td&gt;&lt;b&gt;Mayúsculas+F6 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Descodificar los archivos restantes del directorio&lt;/td&gt;&lt;/tr&gt;.
+  &lt;tr&gt;&lt;td&gt;&lt;b&gt;F7 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Abrir el registro asignado en el visor del sistema operativo&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;F11 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Mover la frecuencia Rx hacia abajo 1 Hz&lt;/td&gt;&lt;td&gt;&lt;b&gt;Ctrl+F11 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Mover las frecuencias Rx y Tx hacia abajo 1 Hz&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;F12 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Subir la frecuencia de Rx 1 Hz&lt;/td&gt;&lt;td&gt;&lt;b&gt;Ctrl+F12 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Subir las frecuencias de Rx y Tx 1 Hz&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+1-6 &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Ajustar ahora la transmisión a este número en la pestaña 1&lt;/td&gt;&lt;/tr&gt;
@@ -5071,7 +5071,7 @@ predefinida. La lista se puede actualizar en Ajustes (F2).</translation>
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+E&lt;/b&gt;&lt;/td&gt;&lt;td&gt;Borrar&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+F &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Saltar todos los filtros de texto&lt;/td&gt;&lt;td&gt;&lt;b&gt;Ctrl+F &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Editar el cuadro de mensaje de texto libre&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+G &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Generar mensajes estándar&lt;/td&gt;&lt;/tr&gt;
-  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+H &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Transmisión de saltos&lt;/td&gt;&lt;/tr&gt;
+  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+H &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Detener TX&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Ctrl+L &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Buscar indicativo en la base de datos, generar mensajes estándar&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+M &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Monitorizar&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+N &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Habilitar Tx&lt;/td&gt;&lt;/tr&gt;
@@ -5079,8 +5079,8 @@ predefinida. La lista se puede actualizar en Ajustes (F2).</translation>
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+S &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Detener la monitorización&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+T&lt;/b&gt;&lt;/td&gt;&lt;td&gt;Sintonizar&lt;/td&gt;&lt;/tr&gt;
   &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+V&lt;/b&gt;&lt;/td&gt;&lt;td&gt;Guardar el archivo *.wav más reciente&lt;/td&gt;&lt;/tr&gt;
-  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+Z &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Filtro, este atajo está siendo soportado en la UI principal y en la UI del widegraph&lt;/td&gt;&lt;/tr&gt;
-  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Esc &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Halt Tx&lt;/td&gt;&lt;/tr&gt;
+  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Alt+Z &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Filtro, este atajo es compatible en la UI principal y en la UI amplia&lt;/td&gt;&lt;/tr&gt;
+  &lt;tr&gt;&lt;td&gt;&lt;b&gt;Esc &lt;/b&gt;&lt;/td&gt;&lt;td&gt;Detener Tx&lt;/td&gt;&lt;/tr&gt;
 &lt;/table&gt;</translation>
     </message>
     <message>
@@ -5134,18 +5134,18 @@ predefinida. La lista se puede actualizar en Ajustes (F2).</translation>
         Doble clic para fijar la frecuencia Rx y decodificar allí.&lt;br/&gt;
         Ctrl-clic para fijar las frecuencias Rx y Tx.&lt;br/&gt;
         Desbloqueado TX=RX:&lt;br/&gt;
-        utilice el botón izq. para fijar la frecuencia de RX&lt;br/&gt;
-        use ALT+botón izq. para fijar la frecuencia de RX y activar el filtro&lt;br/&gt;
-        utilizar el botón derecho para fijar la frecuencia de TX
+        Use el botón izquierdo para fijar la frecuencia de RX&lt;br/&gt;
+        Use ALT+botón izquierdo para fijar la frecuencia de RX y activar el filtro&lt;br/&gt;
+        Use el botón derecho para fijar la frecuencia de TX
     &lt;/td&gt;
   &lt;/tr&gt;
   &lt;tr&gt;
     &lt;td align=&quot;right&quot;&gt;Texto decodificado:&lt;/td&gt;
-    &lt;td&gt;Doble clic para copiar el segundo indicativo en Dx Call,&lt;br/&gt;
-        locator a Dx Grid; cambiar las frecuencias Rx y Tx a&lt;br/&gt;
-        frecuencia de la señal decodificada; generar mensajes estándar.&lt;br/&gt;
-        Si el primer indicativo es el suyo, la frecuencia de Tx no&lt;br/&gt;
-        cambia a menos que se mantenga pulsada la tecla CTRL al hacer doble clic.&lt;br/&gt;&lt;br/&gt;
+    &lt;td&gt;Doble clic para copiar el segundo indicativo en Llamada DX,&lt;br/&gt;
+        locator a locator Dx; cambiar las frecuencias Rx y Tx a frecuencia&lt;br/&gt;
+        de la señal decodificada; generar mensajes estándar.&lt;br/&gt;
+        Si el primer indicativo es el suyo, la frecuencia de Tx no cambia a menos&lt;br/&gt;
+        que se mantenga pulsada la tecla CTRL al hacer doble clic.&lt;br/&gt;&lt;br/&gt;
         ALT+doble clic también detendrá el Tx si el botón Activar Tx está activo.&lt;br/&gt;&lt;br/&gt;
         CTRL+ALT+doble clic sólo añadirá el segundo indicativo del mensaje&lt;br/&gt; decodificado a la lista de indicativos deseados.
      &lt;/td&gt;
@@ -5153,8 +5153,8 @@ predefinida. La lista se puede actualizar en Ajustes (F2).</translation>
   &lt;tr&gt;
     &lt;td align=&quot;right&quot;&gt;Botón de borrar:&lt;/td&gt;
     &lt;td&gt;Pulse el botón derecho para borrar la ventana de QSO.&lt;br/&gt;
-        Haga clic en el botón izq. para borrar la ventana de actividad de banda.&lt;br/&gt;
-        Doble clic botón izq. o dcho. para borrar las ventanas de QSO&lt;br/&gt;
+        Pulse el botón izquierdo para borrar la ventana de actividad de banda.&lt;br/&gt;
+        Doble clic botón izquierdo o derecho para borrar las ventanas de QSO&lt;br/&gt;
         y las ventanas de actividad de banda.
     &lt;/td&gt;
   &lt;/tr&gt;
