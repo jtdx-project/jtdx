df36cc2d29e1aaff9c31f85356d3e7a055a0d82e
Igor Chernikov <c_igor@inbox.ru>
2021-12-01 15:39:45 +0300

    make some more masks

--- lib/ft8apset.f90
+++ lib/ft8apset.f90
@@ -2,7 +2,7 @@ subroutine ft8apset(lmycallstd,lhiscallstd)
 
   use packjt77
   use ft8_mod1, only : apsym,mycall,hiscall,apsymsp,lhound,apsymdxns1,apsymdxns2,mybcall,hisbcall,apcqsym,hisgrid4, &
-                       apsymdxnsrr73,apsymdxns73
+                       apsymdxnsrr73,apsymdxns73,apsymmyns1,apsymmyns2,apsymmynsrr73,apsymmyns73
   character*77 c77
   character*37 msg,msgchk
   character*12 hiscallt,mycallprev,hiscallprev
@@ -15,8 +15,7 @@ subroutine ft8apset(lmycallstd,lhiscallstd)
   data first/.true./
   save hiscallprev,mycallprev,lhoundprev,first
 
-! compiler needs in extra brackets to run this checking
-  if((hiscall.ne.hiscallprev) .or. (mycall.ne.mycallprev) .or. (lhound.neqv.lhoundprev) .or. first) then ! first for lhound triggered
+  if(hiscall.ne.hiscallprev .or. mycall.ne.mycallprev .or. (lhound.neqv.lhoundprev) .or. first) then ! first for lhound triggered
 
     first=.false.; mycallprev=mycall; lhoundprev=lhound
     apsym=0; apsym(1)=99; apsym(30)=99
@@ -64,6 +63,41 @@ subroutine ft8apset(lmycallstd,lhiscallstd)
       endif
     endif
 
+    if(.not.lhound .and. .not.lmycallstd .and. len_trim(mycall).gt.2) then
+      if(lhiscallstd) then
+        msg='<'//trim(mycall)//'> '//trim(hiscall)//' -15'
+        call pack77(msg,i3,n3,c77,0)
+        call unpack77(c77,1,msgchk,unpk77_success,25)
+        if(i3.ne.1 .or. (msg.ne.msgchk .and. trim(msgchk).ne.'<...> '//trim(hiscall)//' -15') .or. .not.unpk77_success) return
+        read(c77,'(58i1)',err=1) apsymmyns2(1:58)
+        apsymmyns2=2*apsymmyns2-1
+
+        nlenmyc=len_trim(mycall)
+        msg=trim(mycall)//' <'//trim(hiscall)//'> RR73'
+        call pack77(msg,i3,n3,c77,0)
+        call unpack77(c77,1,msgchk,unpk77_success,25)
+        if(i3.ne.4 .or. (msgchk(1:nlenmyc+2).ne.(trim(mycall)//' <')) .or. .not.unpk77_success) return
+        read(c77,'(77i1)',err=1) apsymmynsrr73(1:77)
+        apsymmynsrr73=2*apsymmynsrr73-1
+
+        msg=trim(mycall)//' <'//trim(hiscall)//'> 73'
+        call pack77(msg,i3,n3,c77,0)
+        call unpack77(c77,1,msgchk,unpk77_success,25)
+        if(i3.ne.4 .or. (msgchk(1:nlenmyc+2).ne.(trim(mycall)//' <')) .or. .not.unpk77_success) return
+        read(c77,'(77i1)',err=1) apsymmyns73(1:77)
+        apsymmyns73=2*apsymmyns73-1
+
+      else if(lnohiscall) then
+
+        msg='<'//trim(mycall)//'> ZZ1ZZZ -15'
+        call pack77(msg,i3,n3,c77,0)
+        call unpack77(c77,1,msgchk,unpk77_success,25)
+        if(i3.ne.1 .or. (trim(msgchk).ne.'<...> ZZ1ZZZ -15' .and. msgchk.ne.msg) .or. .not.unpk77_success) return
+        read(c77,'(29i1)',err=1) apsymmyns1(1:29)
+        apsymmyns1=2*apsymmyns1-1
+      endif
+    endif
+
     if(len_trim(mycall).lt.3) return
 
     if(.not.lhound .and. lmycallstd .and. (lhiscallstd .or. lnohiscall)) then
@@ -72,7 +106,7 @@ subroutine ft8apset(lmycallstd,lhiscallstd)
       call pack77(msg,i3,n3,c77,0)
       call unpack77(c77,1,msgchk,unpk77_success,25)
 !read(c77(75:77),'(b3)') k3; print *,'i3 =',k3; print *,msgchk
-      if(i3.ne.1 .or. (msg.ne.msgchk) .or. .not.unpk77_success) return
+      if(i3.ne.4 .or. (msg.ne.msgchk) .or. .not.unpk77_success) return
       read(c77,'(58i1)',err=1) apsym(1:58)
       apsym=2*apsym-1; if(lnohiscall) apsym(30)=99
     endif
