c53c5f7f9d113bba9060ef9d051ef284e43f072f
Arvo Järve <arvo.jarve@eesti.ee>
2021-12-28 09:04:23 +0200

    added rigctlcom-jtdx, updated Version

--- Versions.cmake
+++ Versions.cmake
@@ -3,5 +3,5 @@ set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 2)
 set (WSJTX_VERSION_32A 0)
 set (WSJTX_VERSION_SUB 158)
-set (WSJTX_RC 1)		 # release candidate number, comment out or zero for development versions
+set (WSJTX_RC 2)		 # release candidate number, comment out or zero for development versions
 set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
